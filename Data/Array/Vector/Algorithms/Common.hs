b9ac2c5e4a389229b19a720b464ab7e01ab06a78
dan.doel <dan.doel@gmail.com>
2008-07-01 22:56:37 +0000

    Data.Array.Vector.Algorithms.Common added

---
+++ Data/Array/Vector/Algorithms/Common.hs
@@ -0,0 +1,13 @@
+-- ---------------------------------------------------------------------------
+-- |
+-- Module      : Data.Array.Vector.Algorithms.Common
+-- Copyright   : (c) 2008 Dan Doel
+-- Maintainer  : Dan Doel
+-- Stability   : Experimental
+-- Portability : Portable
+--
+-- Common operations and utility functions for all sorts
+
+module Data.Array.Vector.Algorithms.Common where
+
+type Comparison e = e -> e -> Ordering
