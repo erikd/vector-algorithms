cf71761818419b7c11aab7dce73e260192125a49
dan.doel <dan.doel@gmail.com>
2008-07-04 13:50:00 +0000

    Add support for algorithms on immutable vectors.

---
+++ Data/Array/Vector/Algorithms/Immutable.hs
@@ -0,0 +1,23 @@
+{-# LANGUAGE Rank2Types #-}
+
+-- ---------------------------------------------------------------------------
+-- |
+-- Module      : Data.Array.Vector.Algorithms.Immutable
+-- Copyright   : (c) 2008 Dan Doel
+-- Maintainer  : Dan Doel <dan.doel@gmail.com>
+-- Stability   : Experimental
+-- Portability : Non-portable (rank-2 types)
+--
+-- The purpose of this module is to apply the algorithms on mutable arrays
+-- in other packages to immutable arrays. The idea is to copy the immutable
+-- array into a mutable intermediate, perform the algorithm on the mutable
+-- array, and freeze it, yielding a new immutable array.
+
+module Data.Array.Vector.Algorithms.Immutable ( apply ) where
+
+import Control.Monad.ST
+
+import Data.Array.Vector
+
+apply :: (UA e) => (forall s. MUArr e s -> ST s ()) -> UArr e -> UArr e
+apply algo v = newU (lengthU v) (\arr -> copyMU arr 0 v >> algo arr)
