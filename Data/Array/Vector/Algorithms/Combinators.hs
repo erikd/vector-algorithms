a0749d571216a948a241ba4da7d670746605d313
dan.doel <dan.doel@gmail.com>
2009-02-18 10:51:00 +0000

    Tentative schwartzian transform.

--- Data/Array/Vector/Algorithms/Combinators.hs
+++ Data/Array/Vector/Algorithms/Combinators.hs
@@ -1,4 +1,4 @@
-{-# LANGUAGE Rank2Types #-}
+{-# LANGUAGE Rank2Types, TypeOperators #-}
 
 -- ---------------------------------------------------------------------------
 -- |
@@ -18,8 +18,37 @@ module Data.Array.Vector.Algorithms.Combinators ( apply ) where
 
 import Control.Monad.ST
 
+import Data.Ord
+
 import Data.Array.Vector
+import Data.Array.Vector.Algorithms.Common
 
 -- | Safely applies a mutable array algorithm to an immutable array.
 apply :: (UA e) => (forall s. MUArr e s -> ST s ()) -> UArr e -> UArr e
 apply algo v = newU (lengthU v) (\arr -> copyMU arr 0 v >> algo arr)
+
+-- | Uses a function to compute a key for each element which the
+-- algorithm should use in lieu of the actual element. For instance:
+--
+-- > schwartzian sortBy f arr
+--
+-- should produce the same results as:
+--
+-- > sortBy (comparing f) arr
+--
+-- with the schwartzian transform being more efficient (for certain keys), 
+-- because each key is computed only once.
+schwartzian :: (UA e, UA k, Ord k)
+            => (forall e'. Comparison e' -> MUArr e' s -> ST s ())
+            -> (e -> k)
+            -> MUArr e s
+            -> ST s ()
+schwartzian algo f arr = do
+  keys <- newMU (lengthMU arr)
+  fill len keys
+  algo (comparing fstS) (unsafeZipMU keys arr)
+ where
+ len = lengthMU arr
+ fill k keys
+   | k < 0     = return ()
+   | otherwise = readMU arr k >>= writeMU keys k . f >> fill (k-1) keys
