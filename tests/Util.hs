e8afa51a56abcfe795d2a97e8c7a6e29dbfcd978
dan.doel <dan.doel@gmail.com>
2009-05-08 07:00:57 +0000

    Extra Arbitrary instances in Util

--- tests/Util.hs
+++ tests/Util.hs
@@ -1,5 +1,11 @@
+{-# LANGUAGE TypeOperators #-}
 
 module Util where
+
+import Control.Monad
+
+import Data.Word
+import Data.Int
 
 import Data.Array.Vector
 
@@ -8,3 +14,33 @@ import Test.QuickCheck
 
 instance (Arbitrary e, UA e) => Arbitrary (UArr e) where
   arbitrary = fmap toU arbitrary
+
+instance (Arbitrary a, Arbitrary b) => Arbitrary (a :*: b) where
+  arbitrary = (:*:) `fmap` arbitrary `ap` arbitrary
+
+instance Arbitrary Int8 where
+  arbitrary = fromInteger `fmap` arbitrary
+
+instance Arbitrary Int16 where
+  arbitrary = fromInteger `fmap` arbitrary
+
+instance Arbitrary Int32 where
+  arbitrary = fromInteger `fmap` arbitrary
+
+instance Arbitrary Int64 where
+  arbitrary = fromInteger `fmap` arbitrary
+
+instance Arbitrary Word8 where
+  arbitrary = fromInteger `fmap` arbitrary
+
+instance Arbitrary Word16 where
+  arbitrary = fromInteger `fmap` arbitrary
+
+instance Arbitrary Word32 where
+  arbitrary = fromInteger `fmap` arbitrary
+
+instance Arbitrary Word64 where
+  arbitrary = fromInteger `fmap` arbitrary
+
+instance Arbitrary Word where
+  arbitrary = fromInteger `fmap` arbitrary
