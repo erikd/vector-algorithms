c8f0a2c3e8551dd6f64cc06c3acff9c2dcb74413
dan.doel <dan.doel@gmail.com>
2009-09-15 03:29:50 +0000

    Add early-exit binary searches

--- Data/Array/Vector/Algorithms/Search.hs
+++ Data/Array/Vector/Algorithms/Search.hs
@@ -12,7 +12,10 @@
 -- elements into a sorted array.
 
 module Data.Array.Vector.Algorithms.Search
-       ( binarySearchL
+       ( binarySearch
+       , binarySearchBy
+       , binarySearchByBounds
+       , binarySearchL
        , binarySearchLBy
        , binarySearchLByBounds
        , Comparison
@@ -24,6 +27,31 @@ import Data.Bits
 
 import Data.Array.Vector
 import Data.Array.Vector.Algorithms.Common
+
+-- | Finds an index in a givesn sorted array at which the given element could
+-- be inserted while maintaining the sortedness of the array.
+binarySearch :: (UA e, Ord e) => MUArr e s -> e -> ST s Int
+binarySearch = binarySearchBy compare
+
+-- | Finds an index in a given array, which must be sorted with respect to the
+-- given comparison function, at which the given element could be inserted while
+-- preserving the array's sortedness.
+binarySearchBy :: (UA e) => Comparison e -> MUArr e s -> e -> ST s Int
+binarySearchBy cmp arr e = binarySearchByBounds cmp arr e 0 (lengthMU arr)
+
+-- | Given an array sorted with respect to a given comparison function in indices
+-- in [l,u), finds an index in [l,u] at which the given element could be inserted
+-- while preserving sortedness.
+binarySearchByBounds :: (UA e) => Comparison e -> MUArr e s -> e -> Int -> Int -> ST s Int
+binarySearchByBounds cmp arr e l u
+  | u <= l    = return l
+  | otherwise = do e' <- readMU arr k
+                   case cmp e' e of
+                     LT -> binarySearchByBounds cmp arr e (k+1) u
+                     EQ -> return k
+                     GT -> binarySearchByBounds cmp arr e l     k
+ where k = (u + l) `shiftR` 1
+{-# INLINE binarySearchByBounds #-}
 
 -- | Finds the lowest index in a given sorted array at which the given element
 -- could be inserted while maintaining the sortedness.
