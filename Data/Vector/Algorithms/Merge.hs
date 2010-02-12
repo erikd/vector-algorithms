6a473e42708ebbb045f5b85140b2c2a3debc98d6
dan.doel <dan.doel@gmail.com>
2010-02-12 11:09:18 +0000

    Initial port of Merge

--- Data/Vector/Algorithms/Merge.hs
+++ Data/Vector/Algorithms/Merge.hs
@@ -18,40 +18,46 @@ module Data.Vector.Algorithms.Merge
        , Comparison
        ) where
 
-import Control.Monad.ST
+import Prelude hiding (read, length)
+
+import Control.Monad.Primitive
 
 import Data.Bits
-import Data.Array.Vector
-import Data.Array.Vector.Algorithms.Common
+import Data.Vector.Generic.Mutable
 
-import qualified Data.Array.Vector.Algorithms.Optimal   as O
-import qualified Data.Array.Vector.Algorithms.Insertion as I
+import Data.Vector.Algorithms.Common (Comparison, copyOffset)
+
+import qualified Data.Vector.Algorithms.Optimal   as O
+import qualified Data.Vector.Algorithms.Insertion as I
 
 -- | Sorts an array using the default comparison.
-sort :: (Ord e, UA e) => MUArr e s -> ST s ()
+sort :: (PrimMonad m, MVector v e, Ord e) => v (PrimState m) e -> m ()
 sort = sortBy compare
 {-# INLINE sort #-}
 
 -- | Sorts an array using a custom comparison.
-sortBy :: (UA e) => Comparison e -> MUArr e s -> ST s ()
-sortBy cmp arr = sortByBounds cmp arr 0 (lengthMU arr)
+sortBy :: (PrimMonad m, MVector v e) => Comparison e -> v (PrimState m) e -> m ()
+sortBy cmp arr = sortByBounds cmp arr 0 (length arr)
 {-# INLINE sortBy #-}
 
 -- | Sorts a portion of an array [l,u) using a custom comparison.
-sortByBounds :: (UA e) => Comparison e -> MUArr e s -> Int -> Int -> ST s ()
+sortByBounds :: (PrimMonad m, MVector v e)
+             => Comparison e -> v (PrimState m) e -> Int -> Int -> m ()
 sortByBounds cmp arr l u
   | len < 1   = return ()
   | len == 2  = O.sort2ByOffset cmp arr l
   | len == 3  = O.sort3ByOffset cmp arr l
   | len == 4  = O.sort4ByOffset cmp arr l
-  | otherwise = do tmp <- newMU size
+  | otherwise = do tmp <- new size
                    mergeSortWithBuf cmp arr tmp l u
  where
  len  = u - l
  size = (u + l) `div` 2 - l
 {-# INLINE sortByBounds #-}
 
-mergeSortWithBuf :: (UA e) => Comparison e -> MUArr e s -> MUArr e s -> Int -> Int -> ST s ()
+mergeSortWithBuf :: (PrimMonad m, MVector v e)
+                 => Comparison e -> v (PrimState m) e -> v (PrimState m) e
+                 -> Int -> Int -> m ()
 mergeSortWithBuf cmp arr tmp = loop
  where
  loop l u
@@ -64,23 +70,25 @@ mergeSortWithBuf cmp arr tmp = loop
   mid = (u + l) `shiftR` 1
 {-# INLINE mergeSortWithBuf #-}
 
-merge :: (UA e) => Comparison e -> MUArr e s -> MUArr e s -> Int -> Int -> Int -> ST s ()
-merge cmp arr tmp l m u = do memcpyOffMU arr tmp l 0 uTmp
-                             eTmp <- readMU tmp 0
-                             eArr <- readMU arr m
+merge :: (PrimMonad m, MVector v e)
+      => Comparison e -> v (PrimState m) e -> v (PrimState m) e
+      -> Int -> Int -> Int -> m ()
+merge cmp arr tmp l m u = do copyOffset arr tmp l 0 uTmp
+                             eTmp <- read tmp 0
+                             eArr <- read arr m
                              loop 0 eTmp m eArr l
  where
  uTmp = m - l
  uArr = u
  loop iTmp eTmp iArr eArr iIns
    | iTmp >= uTmp = return ()
-   | iArr >= uArr = memcpyOffMU tmp arr iTmp iIns (uTmp - iTmp)
+   | iArr >= uArr = copyOffset tmp arr iTmp iIns (uTmp - iTmp)
    | otherwise    = case cmp eArr eTmp of
-                      LT -> do writeMU arr iIns eArr
-                               eArr <- readMU arr (iArr+1)
+                      LT -> do write arr iIns eArr
+                               eArr <- read arr (iArr+1)
                                loop iTmp eTmp (iArr+1) eArr (iIns+1)
-                      _  -> do writeMU arr iIns eTmp
-                               eTmp <- readMU tmp (iTmp+1)
+                      _  -> do write arr iIns eTmp
+                               eTmp <- read tmp (iTmp+1)
                                loop (iTmp+1) eTmp iArr eArr (iIns+1)
 {-# INLINE merge #-}
 
