1adf0b1c37969887e88469541d26cc82e94c16bf
dan.doel <dan.doel@gmail.com>
2010-02-12 10:15:37 +0000

    Port swap in Common to vector
    + vector has a swap function, but it isn't exported. This is a hold-over
      until that can be fixed.

--- Data/Vector/Algorithms/Common.hs
+++ Data/Vector/Algorithms/Common.hs
@@ -10,23 +10,27 @@
 
 module Data.Vector.Algorithms.Common where
 
-import Control.Monad.ST
+import Prelude hiding (read)
 
-import Data.Array.Vector
+import Control.Monad.Primitive
+
+import Data.Vector.Generic.Mutable
 
 -- | A type of comparisons between two values of a given type.
 type Comparison e = e -> e -> Ordering
 
 -- | Swaps the elements at two positions in an array.
-swap :: (UA e) => MUArr e s -> Int -> Int -> ST s ()
-swap arr i j = do ei <- readMU arr i
-                  readMU arr j >>= writeMU arr i
-                  writeMU arr j ei
+swap :: (PrimMonad m, MVector v e) => v (PrimState m) e -> Int -> Int -> m ()
+swap arr i j = do ei <- read arr i
+                  read arr j >>= write arr i
+                  write arr j ei
 {-# INLINE swap #-}
 
+{-
 mcopyMU :: (UA e) => MUArr e s -> MUArr e s -> Int -> Int -> Int -> ST s ()
 mcopyMU from to iFrom iTo len = go 0
  where
  go n | n < len   = readMU from (iFrom + n) >>= writeMU to (iTo + n) >> go (n+1)
       | otherwise = return ()
 {-# INLINE mcopyMU #-}
+-}
